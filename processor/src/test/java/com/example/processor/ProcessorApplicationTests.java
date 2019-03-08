0067a5d390229b613f517587afe25d9836c7b10a
Adria Navarro <anavarromartin@pivotal.io>
2019-03-08 10:37:55 -0600

    add test, add rabbit starter script

--- processor/src/test/java/com/example/processor/ProcessorApplicationTests.java
+++ processor/src/test/java/com/example/processor/ProcessorApplicationTests.java
@@ -1,16 +1,40 @@
 package com.example.processor;
 
+import lombok.RequiredArgsConstructor;
+import org.jboss.logging.LogMessage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProcessorApplicationTests {
 
+
+	@Autowired
+	private Processor pipe;
+
+	@Autowired
+	private MessageCollector messageCollector;
+
 	@Test
-	public void contextLoads() {
+	public void whenMessageIsReceived_itTransformsItToUpperCase() {
+		pipe.input()
+				.send(MessageBuilder.withPayload("test message")
+						.build());
+
+		Object payload = messageCollector.forChannel(pipe.output())
+				.poll()
+				.getPayload();
+
+		assertEquals("TEST MESSAGE", payload.toString());
 	}
 
 }
